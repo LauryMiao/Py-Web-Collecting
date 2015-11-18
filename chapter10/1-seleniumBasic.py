ea13e0b84fc5f842e1984a8afc50e535c2654d81
Laury Miao <360377765@qq.com>
2015-11-18 10:11:50 +0800

    cookies session and phantomJS

--- chapter10/1-seleniumBasic.py
+++ chapter10/1-seleniumBasic.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 import time
 
-driver = webdriver.PhantomJS(executable_path='')
+driver = webdriver.PhantomJS(
+    executable_path=r'D:/phantomjs-2.0.0-windows/phantomjs-2.0.0-windows/bin/phantomjs')
 driver.get("http://pythonscraping.com/pages/javascript/ajaxDemo.html")
 time.sleep(3)
 print(driver.find_element_by_id("content").text)
